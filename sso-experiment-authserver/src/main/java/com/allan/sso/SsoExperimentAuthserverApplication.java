b39370286a11bc680c778be829e3550af4a0e678
Allan Aquino <allana@buildingiq.com>
2017-04-26 17:39:09 +1000

    first commit

---
+++ sso-experiment-authserver/src/main/java/com/allan/sso/SsoExperimentAuthserverApplication.java
@@ -0,0 +1,12 @@
+package com.allan.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoExperimentAuthserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoExperimentAuthserverApplication.class, args);
+	}
+}
---
+++ sso-experiment-client/src/test/java/com/allan/sso/SsoExperimentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.allan.sso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SsoExperimentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
