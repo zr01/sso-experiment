e86f8c349eb945c416b27e767325ec692642dde7
Allan Aquino <allana@buildingiq.com>
2017-05-02 21:43:13 +1000

    addition of sso-experiment-ui-vue

---
+++ sso-experiment-ui-vue/src/components/Hello.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <ul>
+        <li v-for="user in users">
+          {{user.username}} <-> {{user.email}}
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <input type="text" v-model="input_val" placeholder="fill me...">
+    </div>
+
+    <div>
+      Input Value: <span v-text="input_val"></span>
+    </div>
+
+    <div>
+      <label v-if="ctr % 2 == 0">{{ctr}}</label>
+      <button class="btn btn-primary" v-on:click="ctr++">Clicked {{ctr}} times!</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      users: [
+        { username: 'aatest', email: 'aatest@test.com' },
+        { username: 'abtest', email: 'abtest@test.com' },
+        { username: 'actest', email: 'actest@test.com' }
+      ],
+      input_val: '',
+      ctr: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+    list-style-position: inside;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ sso-experiment-ui-vue/src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="login">
+    <div class="form-group">
+      <label>Username:</label>
+      <input type="text" class="form-control" v-model="username" placeholder="Username">
+    </div>
+    <div class="form-group">
+      <label>Password:</label>
+      <input type="password" class="form-control" v-model="password" placeholder="Password">
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'login',
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
